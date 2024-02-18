a75e9092ee5dc9a7e54b8c73c7649b24b10a33c9
IdoMangadi <idomangadi@gmail.com>
2024-02-18 16:58:13 +0200

    a

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "StrList.h"
+
+int main(){
+    int choice = 0;
+    while(scanf(" %d", &choice) == 0){}
+
+    while(choice != 0){
+        if (choice == 1){
+            char a;
+            scanf(" %d", &a);
+            if (a == 'A'){
+                scanf()
+            }
+        }
+
+    }
+    return 0;
+}
